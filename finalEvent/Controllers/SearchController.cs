5aeb4cf33b474140d32b6db464856d7ddc3f8dd8
robertgoransson <robert.goransson@icloud.com>
2017-04-27 14:18:28 +0200

    searchcontroller

---
+++ finalEvent/Controllers/SearchController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using finalEvent.Models;
+
+namespace finalEvent.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public bool Searcher(String Email)
+        {
+            using (var context = new DbModel())
+              try
+                {
+                    return context.Users.Any(u => u.Email.Equals (Email));
+                    
+                }
+                catch
+                {
+                    return false;
+                }
+            
+            
+        }
+
+    }
+}
