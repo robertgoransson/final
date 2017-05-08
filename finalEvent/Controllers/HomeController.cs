25b6715f4972120196939e34f11f0718ab82b348
Sebastian Ödquist <sebodv151@edunet.oru.se>
2017-05-08 13:37:44 +0200

    ändringar i about

--- finalEvent/Controllers/HomeController.cs
+++ finalEvent/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace finalEvent.Controllers
         
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+           
 
             return View();
         }
         [Authorize]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
         }
