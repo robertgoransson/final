c7bd4dae75404d0e07fab3bee185c20eaca0a390
robertgoransson <robert.goransson@icloud.com>
2017-05-08 14:53:20 +0200

    Mypage skapas

---
+++ finalEvent/Controllers/MyPageController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using finalEvent.Models;
+
+namespace finalEvent.Controllers
+{
+    public class MyPageController : Controller
+    {
+        
+        // GET: MyPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public User GetUser(string Email)
+        {
+            try
+            {
+                using (var context = new DbModel())
+                {
+                    return context.Users.FirstOrDefault(u => u.Email == Email);
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        [Authorize]
+        public ActionResult MyPage()
+        {
+            try
+            {
+                var myUser = User.Identity.Name;
+                var Result = GetUser(myUser);
+                var model = new User
+                {
+                    Firstname = Result.Firstname,
+                    Lastname = Result.Lastname,
+                    Email = Result.Email,
+                    Phonenumber = Result.Phonenumber,
+                    Picture = Result.Picture,
+                    Password = Result.Password
+                };
+
+                return View(model);
+            }
+            catch
+            {
+                return RedirectToAction("About", "Home");
+            }
+        }
+    }
+}
