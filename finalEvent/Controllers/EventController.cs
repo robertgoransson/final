6055b2aeed91add846e46a5c5d56db2d864faf80
Sebastian Ödquist <sebodv151@edunet.oru.se>
2017-05-11 11:01:07 +0200

    början på ett eventskapande

---
+++ finalEvent/Controllers/EventController.cs
@@ -0,0 +1,45 @@
+﻿using finalEvent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace finalEvent.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public ActionResult CreateEvent()
+        {
+            return View();
+        }
+
+        public ActionResult CreateEvent(Event newEvent)
+        {
+            using (var context = new DbModel())
+            {
+                if (!context.Events.Any(e => e.EventId == newEvent.EventId))
+                {
+                    var events = new Event
+                    {
+                        EventName = newEvent.EventName,
+                        Description = newEvent.Description,
+                        StartDate = newEvent.StartDate,
+                        StartHour = newEvent.StartHour,
+                        EndingDate = newEvent.EndingDate,
+                        EndHour = newEvent.EndHour,
+                        Location = newEvent.Location,
+                        Owner = User.Identity.Name.ToString()
+                      
+
+
+                    };
+                    return RedirectToAction("Created", "Home");
+                }
+            }
+
+            return View();
+        }
+    }
+}
